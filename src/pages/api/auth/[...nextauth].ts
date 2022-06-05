429fc0c972824dfc05040fd3f857f00267f8e2a0
Gabriel <gabriel1997.castro@gmail.com>
2022-06-05 15:23:54 -0300

    Save user in FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -8,6 +11,20 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: { params: { scope: "read:user" } },
     }),
-    // ...add more providers here
   ],
+  jwt: {
+    secret: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
